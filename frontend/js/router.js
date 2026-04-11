770023c9dc8695c8c114b5d92c9fffd909850991
manuel <jaureguiimanuel07@gmail.com>
2026-04-11 19:11:13 -0400

    agregación de rutas list y form de cada seccion del dropdown en el router

--- frontend/js/router.js
+++ frontend/js/router.js
@@ -12,8 +12,37 @@ function cargarVista(ruta) {
     // Para cuando esta función se llama, todos los
     // scripts ya cargaron y las funciones existen.
     const rutas = {
-        '/general/DataBase/List/Contacts':   mostrarListado,
-        '/general/DataBase/Create/Contacts': mostrarFormulario,
+        // Clientes
+        '/general/DataBase/List/Customer':   mostrarListadoClientes,
+        '/general/DataBase/Create/Customer': mostrarFormularioClientes,
+
+        // Contactos
+        '/general/DataBase/List/Contacts':   mostrarListadoContactos,
+        '/general/DataBase/Create/Contacts': mostrarFormularioContactos,
+
+        // Servicios
+        '/general/DataBase/List/Services':   mostrarListadoServicios,
+        '/general/DataBase/Create/Services': mostrarFormularioServicios,
+
+        // Epps
+        '/general/DataBase/List/Epps':   mostrarListadoEpps,
+        '/general/DataBase/Create/Epps': mostrarFormularioEpps,
+
+        // Personal
+        '/general/DataBase/List/Staff':   mostrarListadoPersonal,
+        '/general/DataBase/Create/Staff': mostrarFormularioPersonal,
+
+        // Materiales
+        '/general/DataBase/List/Materials':   mostrarListadoMateriales,
+        '/general/DataBase/Create/Materials': mostrarFormularioMateriales,
+
+        // Herramientas
+        '/general/DataBase/List/Tools':   mostrarListadoHerramientas,
+        '/general/DataBase/Create/Tools': mostrarFormularioHerramientas,
+
+        // Ofertas
+        '/general/DataBase/List/Offers':   mostrarListadoOfertas,
+        '/general/DataBase/Create/Offers': mostrarFormularioOfertas,
     }
 
     const vista = rutas[ruta]
